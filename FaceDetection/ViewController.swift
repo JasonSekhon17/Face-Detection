06b08175ed699e251b73190a174ea7f9acc8b512
JasonSekhon <jason_2s@hotmail.com>
2017-10-25 18:52:05 -0700

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Jason Sekhon on 2017-10-25.
+//  Copyright © 2017 Jason Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
